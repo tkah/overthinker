0d12d98a4fb8a46e4b843af71e4f9fe49521be41
spholman <sidholman@unm.edu>
2014-11-11 14:55:40 -0700

    inital model mockup

---
+++ src/model/Model.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sidholman on 11/11/14.
+ */
+public class Model {
+
+    private static int distortionValue = 0;
+    private static int gameMode = 0;
+    private static ArrayList<float[]> playerLocations;
+
+
+    public static void Model() {
+
+    }
+
+    public static synchronized void setDistortionValue(int val) {
+        distortionValue = val;
+    }
+
+    public static int getDistortionValue() {
+        return distortionValue;
+    }
+
+    public static synchronized void setClientPosition(int playerNumber, float[] position) {
+        float[] toSet = playerLocations.get(playerNumber);
+        for (int i = 0; i < 3; i++)
+            toSet[i] = position[i];
+    }
+
+    public static float[] getClientPosition(int playerNumber) {
+        return playerLocations.get(playerNumber);
+    }
+}
