a7ca5c1305a8727e2be7feec1b8cb5cf7d8d6cae
spholman <spholman@unm.edu>
2014-12-07 21:39:27 -0700

    Tested EEGLogger on Win7, fixed bugs.

--- src/overthinker/client/eeg/EEGLogger.java
+++ src/overthinker/client/eeg/EEGLogger.java
@@ -7,7 +7,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * Created by sidholman on 12/7/14.
+ * Optional class, will write the EEG short-term excitement levels to
+ * a time-stamped file in the current working directory.
  */
 public class EEGLogger {
 
@@ -18,15 +19,23 @@ public class EEGLogger {
     private FileOutputStream outStream = null;
     private OutputStreamWriter out = null;
 
+
+    /**
+     * Sets up a time-stamped file for future writing.
+     * Save path varies depending on OS--windows 7 saves
+     * to Public Documents, everything else saves to
+     * current working directory.  EEG only runs on Win,
+     * so to avoid clutter I chose to change save path.
+     */
     public EEGLogger() {
 
-        DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd_HH:mm:ss");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
         Date date = new Date();
-        timestamp = dateFormat.format(date).toString();//date.toString();
+        timestamp = dateFormat.format(date).toString();
 
         osType = System.getProperty("os.name");
-        if (osType.contains("win") && osType.contains("7")) {
-            logPath = "C:\\Users\\Public\\Public Documents\\";
+        if (osType.contains("Win") && osType.contains("7")) {
+            logPath = "C:\\Users\\Public\\Documents\\"; //To avoid cluttering working directory
             logFile = new File(logPath + timestamp + ".txt");
         } else {
             logPath = System.getProperty("user.dir") + "/" + timestamp + ".txt";
@@ -41,14 +50,22 @@ public class EEGLogger {
         }
     }
 
+    /**
+     * Writes the given string to the current file,
+     * concatenated with sys-independent newline char.
+     * @param line String to write
+     */
     public void writeLine(String line) {
         try {
-            out.write(line+"\n");
+            out.write(line+System.lineSeparator());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     *Should be called when EEG is shut down.
+     */
     public void closeStreams() {
         try {
             out.close();
@@ -56,11 +73,11 @@ public class EEGLogger {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     //TODO remove main method before submission--it's for testing only.
     public static void main(String[] args) {
+        System.out.println(System.getProperty("os.name"));
         EEGLogger logger = new EEGLogger();
         for (int i = 0; i < 100; i++) {
             logger.writeLine(i+"-ith 'Hello' entry.");
