ae6b93abc14a46b1842382d2b30ef0d64f09bc99
TorranK <tkahleck@unm.edu>
2014-12-09 12:23:19 -0700

    map image

--- src/overthinker/client/GamePlayAppState.java
+++ src/overthinker/client/GamePlayAppState.java
@@ -460,7 +460,7 @@ public class GamePlayAppState extends AbstractAppState
   {
     try
     {
-      netClient = Network.connectToServer("10.80.35.217", 6143);
+      netClient = Network.connectToServer("192.168.1.121", 6143);
     }
     catch (IOException e)
     {
