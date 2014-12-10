548a87840fee60f437e78401d0f421e5a9f30ddd
Peter Ortegel <p.s.ortegel@gmail.com>
2014-12-09 18:50:50 -0700

    Commenting network package

--- src/overthinker/net/ChangeWaterRateRequest.java
+++ src/overthinker/net/ChangeWaterRateRequest.java
@@ -4,18 +4,33 @@ import com.jme3.network.AbstractMessage;
 import com.jme3.network.serializing.Serializable;
 
 /**
- * Created by Peter on 12/5/2014.
+ * This class is used to send a water rate update over the network. Only used by the Otherthinker.
+ *
+ * @author Peter, Josh, Sid, Derek, and Torran.
  */
 @Serializable
 public class ChangeWaterRateRequest extends AbstractMessage {
     private float waterRate;
 
+    /**
+     * Empty constructor required by Serializable.
+     */
     public ChangeWaterRateRequest() {}
 
+    /**
+     * Gets the new water rate.
+     *
+     * @return - water rate.
+     */
     public float getWaterRate() {
         return waterRate;
     }
 
+    /**
+     * Sets the new water rate.
+     *
+     * @param waterRate - new water rate.
+     */
     public void setWaterRate(float waterRate) {
         this.waterRate = waterRate;
     }
--- src/overthinker/net/PlayerDeathRequest.java
+++ src/overthinker/net/PlayerDeathRequest.java
@@ -4,9 +4,14 @@ import com.jme3.network.AbstractMessage;
 import com.jme3.network.serializing.Serializable;
 
 /**
- * Created by Peter on 12/5/2014.
+ * This class is used to send a client death notification over the network.
+ *
+ * @author Peter, Torran, Derek, Josh and Sid.
  */
 @Serializable
 public class PlayerDeathRequest extends AbstractMessage {
+    /**
+     * Empty constructor used by Serializable.
+     */
     public PlayerDeathRequest() {}
 }
